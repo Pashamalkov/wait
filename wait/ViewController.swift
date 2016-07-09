c591a3ac177dcd2d698037f055ed562a8b825c31
mikhailarzumanov <moarzumanov@edu.hse.ru>
2016-07-09 15:27:05 +0300

    Initial Commit

---
+++ wait/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wait
+//
+//  Created by Mikhail  Arzumanov on 09.07.16.
+//  Copyright © 2016 Mikhail  Arzumanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
